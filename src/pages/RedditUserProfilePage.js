3b7db0d15d8aa433fa070ec07bf34db0facd0331
izzy1090 <michaelisabella1@gmail.com>
2023-03-06 19:52:17 -0500

    Provided secure storage for refreshToken
    We can now grab the refresh token from cookies and have them securely
    stored. By using js-cookie, we can securely store our refreshToken
    in a cookie and easily grab it after page refreshes to ensure we
    always have the most current refreshToken.
    
    The userProfile is still stored locally because it does not contain
    any private data.

--- src/pages/RedditUserProfilePage.js
+++ src/pages/RedditUserProfilePage.js
@@ -1,8 +1,10 @@
+import Cookies from "js-cookie";
 import RedditUserProfile from "../components/RedditUserProfile";
 import fetchAccessToken from '../api/FetchAccessToken';
 import fetchUserProfile from '../api/FetchUserProfile';
 import { useEffect, useState } from "react";
 import FetchSubredditsList from "../components/FetchSubredditsList";
+
 
 function RedditUserProfilePage({ refreshToken, authToken, handleRefreshToken }){
     const [ userProfile, setUserProfile ] = useState({});
@@ -11,16 +13,17 @@ function RedditUserProfilePage({ refreshToken, authToken, handleRefreshToken }){
     // this render the landingPage only after the user has authorized the application
     // to have access to their Reddit account
     useEffect(()=>{
-        // this fetches the user info and logs to the console the available access tokens
-        // and refresh tokens as well as logs to the console the reddit user's info
+        /* This fetches the user info and logs to the console the available access tokens
+        and refresh tokens as well as logs to the console the reddit user's info */
         const fetchUserInfo = async (token) => {
             try {
                 setLoading(true);
                 const request = await fetchAccessToken(token);
+                // sets the refresh token in case we need it for immediate use after loading the profile
                 handleRefreshToken(request.refresh_token);
-                sessionStorage.setItem('refreshToken', JSON.stringify(request.refresh_token))
                 const response = await fetchUserProfile(request.access_token);
-                
+                // also securely stores the refresh token in a cookie and protects against cross site attacks
+                Cookies.set('refreshToken', request.refresh_token, { expires: 1/24, secure: true, sameSite: true });
                 return response;
             } catch (error){
                 console.log('Error fetching credentials: ', error);
@@ -35,14 +38,15 @@ function RedditUserProfilePage({ refreshToken, authToken, handleRefreshToken }){
                 
             })
         } else {
-            // else we want to init. userData with the stored session data associated with userProfile
-            // and if the userData has a value we want to set the userProfile state to be the userData
+            // we then store our userData is sessionStorage as it doesn't contain any confidential information
             const userData = JSON.parse(sessionStorage.getItem('userProfile'))
-            const cachedToken = JSON.parse(sessionStorage.getItem('refreshToken'))
-            if (userData && cachedToken){
+            // also grabs the refresh token from our cookie
+            const cachedToken = Cookies.get('refreshToken');
+            if (userData && cachedToken){  
+                // then sets both the userData and refreshTokens to stored values from last page render
                 setUserProfile(userData)
                 handleRefreshToken(cachedToken)
-            }
+            } 
         }
     },[authToken, handleRefreshToken])
 
