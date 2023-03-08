9a7808ff7c16ed9174e11f1adae22ea18cef565b
izzy1090 <michaelisabella1@gmail.com>
2023-03-08 16:28:18 -0500

    Successfully setup api call to Subscribed Reddits
    Able to make an api call to the necessary API endpoint. Next step
    is to decide what additional api endpoints you want to make calls
    to and how you want to visualize the returned data.

--- src/components/SubscribedReddits.js
+++ src/components/SubscribedReddits.js
@@ -1,10 +1,18 @@
 function SubscribedReddits({ subscribedSubreddits }){
     if (subscribedSubreddits.length !== 0){
         const renderedSubscribedSubreddits = subscribedSubreddits.data.children.map((subreddit, i)=>{
-            return <div key={subreddit[i]}>{subreddit}</div>
+            if (!subreddit.data[undefined]){
+                return <div key={i}>
+                    <img src={subreddit.data.icon_img} alt=''/>
+                    <div>Subreddit Name: {subreddit.data.display_name_prefixed}</div>
+                    <div>Description: {subreddit.data.public_description}</div>
+                    <div>Subscribers: {subreddit.data.subscribers}</div>
+                    <div>Category: {subreddit.data.advertiser_category}</div>
+                    <div>Favorited By User: {subreddit.data.user_has_favorited}</div>
+                </div>
+            } else return <div>No content found...</div>;
         })
-        console.log(renderedSubscribedSubreddits)
-        return<div>Subscribed Reddits</div>
+        return<div>{renderedSubscribedSubreddits}</div>
     }
     
     
