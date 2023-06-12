5776a9cdba58c5898704d72d6a8214c676de8f45
Michael Isabella <michaelisabella1@gmail.com>
2023-06-11 23:20:12 -0400

    Added ref. to env. variables for next js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+      REACT_APP_CLIENT_SECRET: process.env.REACT_APP_CLIENT_SECRET, 
+      REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI, 
+      REACT_APP_RANDOM_STRING: process.env.REACT_APP_RANDOM_STRING
+    }
+  }
+  
+  module.exports = nextConfig
