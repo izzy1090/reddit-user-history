798a94bcfe54347fa7e5ef0206d5550a4bce1234
Michael Isabella <michaelisabella1@gmail.com>
2023-06-11 23:25:36 -0400

    updated env var

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-      APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
-      REACT_APP_CLIENT_SECRET: process.env.REACT_APP_CLIENT_SECRET, 
-      REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI, 
-      REACT_APP_RANDOM_STRING: process.env.REACT_APP_RANDOM_STRING
+        REACT_APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+        REACT_APP_CLIENT_SECRET: process.env.REACT_APP_CLIENT_SECRET, 
+        REACT_APP_REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI, 
+        REACT_APP_RANDOM_STRING: process.env.REACT_APP_RANDOM_STRING
     }
   }
   
